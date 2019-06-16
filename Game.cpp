f6df4f4a1f8d69e452d6b4ea03896d279800a1ff
Daniel Widgren <daniel@zaark.com>
2019-06-16 21:25:00 +0200

    now draws an image on screen

--- Game.cpp
+++ Game.cpp
@@ -1,5 +1,9 @@
 #include "Game.h"
 
+SDL_Texture* playerTex;
+SDL_Rect srcR, destR;
+
+
 Game::Game()
 {}
 Game::~Game()
@@ -8,29 +12,41 @@ Game::~Game()
 void Game::init(const char *title, int xpos, int ypos, int width, int height, bool fullscreen)
 {
   int flags = 0;
+
   if(fullscreen) {
       flags = SDL_WINDOW_FULLSCREEN;
   }
   
   if(SDL_Init(SDL_INIT_EVERYTHING) == 0) {
-      std::cout << "Subsystems Initilised..." << std::endl;
-
-      window = SDL_CreateWindow(title, xpos, ypos, width, height, flags);
-      if(window){
-	  std::cout << "Window created..." << std::endl;
-      }
-
+    if (!(IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG)) {
+      printf("could not initialize sdl2_image: %s\n", IMG_GetError());
+    }
+    std::cout << "Subsystems Initilised..." << std::endl;
+    
+    window = SDL_CreateWindow(title, xpos, ypos, width, height, flags);
+    if(window){
+      std::cout << "Window created..." << std::endl;
+    }
+    
       renderer = SDL_CreateRenderer(window, -1, 0);
       if(renderer){
-	SDL_SetRendererDrawColor(renderer, 255, 255, 255, 255);
+	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 	std::cout << "Renderer created..." << std::endl;
       }
-
+      
       isRunning = true;
       
   } else {
     isRunning = false;
   }
+
+  SDL_Surface* tmpSurface = IMG_Load("assets/player.png");
+  if(tmpSurface == NULL) {
+      printf("could not get image: %s\n", IMG_GetError());
+  }
+  
+  playerTex = SDL_CreateTextureFromSurface(renderer, tmpSurface);
+  SDL_FreeSurface(tmpSurface);
 }
 
 void Game::handleEvents()
@@ -47,12 +63,17 @@ void Game::handleEvents()
 }
 
 void Game::update()
-{}
+{
+  destR.h = 32;
+  destR.w = 32;
+  
+}
 
 void Game::render()
 {
-  SDL_RendererClear(renderer);
-  SDL_RendererPresent(renderer);
+  SDL_RenderClear(renderer);
+  SDL_RenderCopy(renderer, playerTex, NULL, &destR);
+  SDL_RenderPresent(renderer);
 }
 
 void Game::clean()
--- Game.h
+++ Game.h
@@ -1,5 +1,7 @@
-#include "SDL.h"
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 #include <iostream>
+#include <string>
 
 class Game {
  public:
