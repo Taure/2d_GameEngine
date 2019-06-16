f2601bb80476ef1057abc041832eb0d02dd2ea00
Daniel Widgren <daniel@zaark.com>
2019-06-16 21:59:51 +0200

    added keyboard movements

--- Game.cpp
+++ Game.cpp
@@ -2,7 +2,7 @@
 
 SDL_Texture* playerTex;
 SDL_Rect srcR, destR;
-
+const Uint8 *keystate;
 
 Game::Game()
 {}
@@ -54,12 +54,22 @@ void Game::handleEvents()
   SDL_Event event;
   SDL_PollEvent(&event);
   switch (event.type) {
-    case SDL_QUIT:
+  case SDL_QUIT:
+    isRunning = false;
+    break;
+    /*  case SDL_KEYDOWN:
+    switch (event.key.keysym.sym) {
+    case SDLK_ESCAPE:
+    case SDLK_q:
       isRunning = false;
       break;
-    default:
-      break;
     }
+    break;*/
+  default:
+    break;
+  }
+
+
 }
 
 void Game::update()
